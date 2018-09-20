94edf8e40710002b3a8ac0fd1e561b49b37a4395
tone <tone4all@163.com>
2018-09-20 22:25:34 +0800

    aop 运行 minstrel

--- src/main/java/com/simba/springbase/knights/KnightMain.java
+++ src/main/java/com/simba/springbase/knights/KnightMain.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class KnightMain {
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knight.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/Minstrel.xml");
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
         context.close();
