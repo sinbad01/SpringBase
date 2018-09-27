4e2c52c06bca76e6a31a0ec1c74f0d9542b5616e
tone <tone4all@163.com>
2018-09-27 22:02:49 +0800

    组件扫描的更详细配置

--- src/main/java/com/simba/springbase/soundsystem/CDPlayerConfig.java
+++ src/main/java/com/simba/springbase/soundsystem/CDPlayerConfig.java
@@ -3,10 +3,13 @@ package com.simba.springbase.soundsystem;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+//通过代码启用组件扫描，与SoundSystem.xml 起到同样的作用
+
 //@ComponentScan注解， 这个注解能够在Spring中启用 组件扫描。
 //@ComponentScan默认会扫描与配置类相同的包, 这样就能发现CompactDisc， 并且会在Spring中自动为其创建一个bean。
+// 可以指定扫描的包名
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages = "com.simba.springbase.soundsystem")
 public class CDPlayerConfig {
 }
--- src/main/java/com/simba/springbase/soundsystem/SgtPeppers.java
+++ src/main/java/com/simba/springbase/soundsystem/SgtPeppers.java
@@ -3,7 +3,8 @@ package com.simba.springbase.soundsystem;
 import org.springframework.stereotype.Component;
 
 // @Component 注解表明该类会作为组件类， 并告知Spring要为这个类创建bean
-@Component
+// 为 bean 命名
+@Component("lonelyHeartsClub")
 public class SgtPeppers implements CompactDisc {
 
     private String title = "Sgt. Pepper's Lonely Hearts Club Band";
